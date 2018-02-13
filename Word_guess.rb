c31daa3fe6041a6e960acb1780fb914febf7c356
Kiera <kiera.thomasson@gmail.com>
2018-02-13 15:16:54 -0800

    reorganized code, added photo graphic to print, and win lo/lose message

--- Word_guess.rb
+++ Word_guess.rb
@@ -1,5 +1,4 @@
 
-
 class SecretWord
 attr_accessor :word_template, :secret_word
 # generate secret word
@@ -15,25 +14,6 @@ class Visual
   def initialize(photo)
     @photo = photo
   end
-# change visual based on incorrect guesses (get smaller, lose a piece, do something)
-  # def print_photo(wrong_guess_count)
-  #   case wrong_guess_count
-  #   when == 0
-  #     print photo 5/5
-  #   when == 1
-  #     print photo 4/5
-  #   when == 2
-  #     print photo 3/5
-  #   when == 3
-  #     print photo 2/5
-  #   when == 4
-  #     print 1/5
-  #   when == 5
-  #     print "Game Over!"
-  # end
-
-# optional print sentence of number of guesses left
-
 
 end
 
@@ -125,11 +105,15 @@ picture_5 = "
                `.   ,-._          `.   ,-._          `.   ,-._
                   `-'                `-'               `-'     "
 
-puts picture_1
-puts picture_2
-puts picture_3
-puts picture_4
-puts picture_5
+# puts picture_1
+# puts picture_2
+# puts picture_3
+# puts picture_4
+# puts picture_5
+
+
+
+# optional print sentence of number of guesses left
 
 word_pool = [
   "plant",
@@ -179,30 +163,26 @@ end
 #   end
 # end
 
+
+
+# beginning of user interface Welcome user to game
+puts "Welcome to K & H Word Guess"
+puts "To play the game you will guess our secret word one letter at a time."
+puts "You start out with 5 incorrect guess. USE THEM WISELY\n"
+puts picture_5
+
 random_word = get_random_word(word_pool)
-puts random_word
 
-# display length of word with  _ _
-# method to determine length of secret_word
-# and print _ _ for the number of letters
+puts random_word #this is a tester
 
+# determine length of secret_word
 # def create_word_template(random_word)
 word_length = random_word.length
 word_template = Array.new(word_length, "_" )
 # end
 
 split_word = random_word.split('')
-
-# create_word_template(random_word)
-# puts word_template
-
-# Welcome user to game
-puts "Welcome to K & H Word Guess"
-puts "To play the game you will guess our secret word one letter at a time."
-puts "You start out with 5 incorrect guess. USE THEM WISELY\n"
-
 # display visual and length of word
-
 print word_template
 puts "\n\n"
 
@@ -212,42 +192,49 @@ while incorrect_guesses.length < 5
   guess = gets.chomp
 
   if random_word.include? "#{guess}"
-    puts "the word contains the guess"
+    puts "the word contains #{guess}"
     check_guess(split_word, guess, word_template)
+    if word_template.include?("_") == false
+      puts "Congratulations - you win!!"
+    end
+# *** allow user to win
+# — if all letters guessed before max of guess left
+# — if user wins print secret word and congratulate
+
   else
     incorrect_guesses << guess
-    puts " The current incorrect guesses are #{incorrect_guesses}"
   end
+# *** reprint visual and incorrect_guesses
+  wrong_guess_count = incorrect_guesses.length
+
+  # change visual based on incorrect guesses (get smaller, lose a piece, do something)
+  case wrong_guess_count
+  when 0
+    print picture_5
+  when 1
+    print picture_4
+  when 2
+    print picture_3
+  when 3
+    print picture_2
+  when 4
+    print picture_1
+  when  5
+    print "Game Over! The word was #{random_word}!"
+  end
+  puts "\n\n"
+  puts "You have #{(5 - wrong_guess_count)} guess(es) left!"
+  puts "\n\n"
+  puts "The word doesn't include: #{incorrect_guesses}"
+  puts "\n\n"
   puts "#{word_template}"
   puts "\n\n"
 end
+# *** allow user to lose
+# — if reach max guesses#
+# - losing message
+# if user loses print answer to secret word
 
 
 
-
-
-# *** losing message
-
-# # update secret word template
-# # reprint visual and incorrect_guesses
-#
-
-# # reprint secret word template
-# # update and print visual and  print incorrect_guesses
-
-
-# 2. Allow user to guess letter by letter to determine secret word - puts and gets.chomp
-#
-# 3. if letter is apart of secret word display correct location within word and blank _ for remaining letters
-#
-# 4. if letter is not apart of the word reprint blank __ word
-#
-# 5. allow user to win
-# — if all letters guessed before max of guess left
-#
-# 6. allow user to lose
-# — if reach max guesses
-#
-# 7. if user loses print answer to secret word
-# — if user wins print secret word and congratulate
 # —  ask user if they want to play again and start game over if yes
